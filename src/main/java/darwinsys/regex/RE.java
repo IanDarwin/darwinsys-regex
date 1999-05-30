823425ff6d8d45847a84fa1623a575f447c1237e
IanDarwin <>
1999-05-30 13:59:06 +0000

    First attempt at converting from "compile into StringBuffer" into "compile into list of SE (sub-expression) objects".

--- src/main/java/darwinsys/regex/RE.java
+++ src/main/java/darwinsys/regex/RE.java
@@ -1,17 +1,26 @@
+import java.util.*;
+
 /**
  * Another attempt at a toy regular expressions package for Java.
  *
  * This is a very simple implementation, compiling the pattern
- * into a StringBuffer, and interpreting it. As Henry Spencer
+ * into an array of SE objects, and interpreting it. As Henry Spencer
  * said of his (much more sophisticated) C-language regexp,
  * replacing the innards of egrep with this code would be a mistake.
  *
- * NOT YET WORKING -- need to finish converting from C,
- * especially the places where an int is computed & should be returned
- * but a boolean is the actual return type. Also several charAt(i)==\0!
+ * NOT FINISHED. TODO:
+ * Get doMatch() working for all types.
+ * Get compile() to do closures (of all 3 types).
+ * Write SEclos.amatch().
+ * Fix all the places that have XXX.
+ *
+ * More Functionality:
+ * Write versions that return Match(), not just boolean version.
+ * Use that to Write sub() functionality.
+ * Add Perl-style REs \w \n etc.
  *
  * @author Ian F. Darwin, ian@darwinsys.com.
- * Patterned after a version I wrote years ago in C as part of a
+ * Based in part on a version I wrote years ago in C as part of a
  * text editor implementation, in turn based on Kernighan & Plaughers
  * <I>Software Tools In Pascal</I> implementation.
  *
@@ -32,15 +41,14 @@ public class RE {
 	public static final char OR = |;			// XXX
 	public static final char GRP = (;			// XXX
 	public static final char GRPEND = );		// XXX
-	public static final char WORD = w;
+	public static final char WORD = w;		// XXX Perlish
+	public static final char NUMBER = n;		// XXX Perlish
 	public static final char LITCHAR = \\;
 	public static final char NEGATE = ^;
-	public static final char NCCL = x;
-	protected static final int CLOSIZE = 1;
 	public static final char DASH = -;
 	final int ERR = -1;
 
-	protected StringBuffer myPat;
+	protected SE[] myPat;
 	protected String origPatt;
 
 	/** Construct an RE object, given a pattern.
@@ -52,8 +60,16 @@ public class RE {
 	}
 
 	public String toString() {
-		return "RE[" + origPatt + "-->" + myPat + "]";
+		StringBuffer res = new StringBuffer("RE[" + origPatt + "-->");
+		for (int i=0; i<myPat.length; i++) {
+			res.append(myPat[i]);
+			res.append(,);
+		}
+		res.append(]);
+		return res.toString();
 	}
+
+	protected static RE singleton;
 
 	/** Match a pattern in a given string. Designed for light-duty
 	 * use, as it compiles the pattern each time.
@@ -61,8 +77,10 @@ public class RE {
 	 * which stores the pattern inside it.
 	 */
 	public static Match match(String patt, String str){
-		StringBuffer thispat = compile(patt);
-		// return match(thispat, str);
+		if (singleton == null)
+			singleton = new RE("");
+		SE[] thispat = singleton.compile(patt);
+		//return singleton.match(thispat, str);
 		return null;
 	}
 
@@ -70,7 +88,7 @@ public class RE {
 	 * @return True if the RE matches anyplace in the String.
 	 */
 	public boolean isMatch(String str){
-		return match(myPat, str);
+		return doMatch(myPat, str);
 	}
 
 	/** Match the compiled pattern stored in the RE in a given String.
@@ -95,216 +113,69 @@ public class RE {
 	 * @return compiled pattern
 	 * @throws RESyntaxException if bad syntax.
 	 */
-	protected static StringBuffer compile(String arg) throws RESyntaxException {
-		Int i = new Int();		// arg index, initially 0
+	protected SE[] compile(String arg) throws RESyntaxException {
+		Int i = new Int();	// arg index
 		int j = 0,	// patt "index"
 			lastj = 0, lj = 0;
 		boolean done = false;
-
-		StringBuffer patt = new StringBuffer(arg.length()*2); // guess length
+		Vector v = new Vector();
 
 		while (!done && i.get()<arg.length()) {
 			char c = arg.charAt(i.get());
 			lj = j;
 			if (c == ANY) {
-				patt.append(ANY);
-			// "^" only special at beginning/
+				v.addElement(new SEany());
+			// "^" only special at beginning.
 			} else if (c == BOL && i.get() == 0) {
-				patt.append(BOL);
-			// "$" only special at end */
-			} else if (c == EOL && i.get() > 0) {
-				patt.append(EOL);
+				v.addElement(new SEbol());
+			// "$" only special at end.
+			} else if (c == EOL && i.get() == arg.length()-1) {
+				v.addElement(new SEeol());
 			} else if (c == CCL) {
-				if (getCCL(arg, i, patt) == false)
-					break;
+				v.addElement(new SEccl(arg, i));
 			// "*" not special unless it follows something
-			} else if (c == CLOSURE_ANY && i.get() > 0) {
-				lj = lastj;
-				if (patt.charAt(lj) == BOL || patt.charAt(lj) == EOL ||
-					patt.charAt(lj) == CLOSURE_ANY)
-					break;	/* terminate loop */
-				else
-					/* replaces stclose: lastj==where orig. pattern began */
-					patt.insert(lastj, CLOSURE_ANY);
-					++j;
+		//
+		//	} else if (c == CLOSURE_ANY && i.get() > 0) {
+		//		lj = lastj;
+		//		if (patt.charAt(lj) == BOL || patt.charAt(lj) == EOL ||
+		//			patt.charAt(lj) == CLOSURE_ANY)
+		//			break;	/* terminate loop */
+		//		else
+		//			/* replaces stclose: lastj==where orig. pattern began */
+		//			patt.insert(lastj, CLOSURE_ANY);
+		//			++j;
 			} else {
-				// "Ordinary" char, but must be LITCHARd so we dont
-				// mix it up with ^ ! x etc.
-				patt.append(LITCHAR);		++j;
-				patt.append(esc(arg, i));	++j;	// XXX patsize?
+				// "Ordinary" character.
+				v.addElement(new SEchar(esc(arg, i)));
 			}
 			lastj = lj;
 			if (!done) i.incr();
 		} 
 		if (done) {				/* finished early */
 			throw new RESyntaxException("incomplete pattern");
-		} else
-			return patt;
+		} 
+		SE[] a = new SE[v.size()];
+		v.toArray(a);
+		return a;
 	}
 
-	/** getCCL -- expand char class at arg[i] into pat[j].
-	 * @return true if pattern OK, false if a problem.
+
+	/* doMatch -- find pattern match anywhere on line.
+	 * The true inner heart of the matching engine.
 	 */
-	protected static boolean getCCL(String arg, Int i, StringBuffer patt) {
-		int jstart;
-
-		i.incr();			/* skip over [ */
-		if (i.get()>=arg.length())
-			return false;
-		if (arg.charAt(i.get()) == NEGATE) {
-			patt.append(NCCL);
-			i.incr();
-		} else
-			patt.append(CCL);
-		jstart = patt.length();
-		// Expand the range
-		int len = doDash(CCLEND, arg, i, patt);
-		// store the length before it: cast int to 16-bit char.
-		patt.insert(jstart, (char)(len));
-		return arg.charAt(i.get()) == CCLEND;
-	}
-
-	/* doDash - expand dash shorthand set at src[i] to end of dest.
-	 * @return number of characters appended to dest.
-	 */
-	protected static int doDash(
-		char dlm, String src, Int i, StringBuffer dest) {
-
-		int startLen = dest.length();
-
-		while (src.charAt(i.get()) != dlm && i.get()<src.length()) {
-			if (src.charAt(i.get()) == LITCHAR)
-				dest.append(esc(src, i));
-			else if (src.charAt(i.get()) != DASH)
-				dest.append(src.charAt(i.get()));
-			else if (dest.length() == 0 || src.length() == i.get()+1)
-				dest.append(DASH);	/* literal - */
-			else if (Character.isLetterOrDigit(src.charAt(i.get()-1)) && 
-				Character.isLetterOrDigit(src.charAt(i.get()+1)) &&
-				src.charAt(i.get()-1) <= src.charAt(i.get()+1)) {
-				for (int k = src.charAt(i.get()-1)+1; k <= src.charAt(i.get()+1); k++)
-					dest.append(k);
-				i.incr();
-			}
-			else
-				dest.append(DASH);
-			i.incr();
-		}
-		return dest.length() - startLen;
-	}
-
-	/* match -- find pattern match anywhere on line */
-	protected boolean match(StringBuffer patt, String line) {
-		int	i = 0, j = 0;
-
-		while (i<line.length() && j == 0) {
-			j = amatch(line, i, patt, 0);
-			i++;
-		}
-		return j > 0;
-	}
-
-	/* amatch - look for match of patt[j]... at lin[offset]... */
-	int amatch(String line, int offset, StringBuffer patt, int j) {
-		Int i = new Int(offset);
-		int k = 0;
+	protected boolean doMatch(SE[] patt, String line) {
+		Int	i = new Int(0);
 		boolean done = false;
 
-		while (!done && j<patt.length())
-			if (patt.charAt(j) == CLOSURE_ANY) {
-				j += CLOSIZE;
-				i.set(offset);
-				/* match as many as possible */
-				while (!done && i.get()<line.length())
-					if (omatch(line, i, patt, j) != true)
-						done = true;
-				/*
-				 * i points to input char that made omatch fail.
-				 * match rest of pattern against rest of input.
-				 * shrink closure by 1 after each failure.
-				 */
-				done = false;
-				while (!done && i.get() >= offset) {
-					k = amatch(line, i.get(), patt, j+patsize(patt, j)); // recurse
-					if (k > 0)	/* matched rest of pattern */
-						done = true;
-					else
-						i.decr();
+		for (int il=0; il<line.length(); il++) {
+			for (int ip=0; ip<patt.length; ip++) {
+				if (!patt[ip].amatch(line, i)) {
+					done = true;
+					break;		// out of inner loop
 				}
-				offset = k;	/* if k=0 failure, else success */
-				done = true;
 			}
-			// XXX should i.get be i, and change the header?
-			else if (omatch(line, i, patt, j) != true) {
-				offset = 0;	/* non-closure */
-				done = true;
-			}
-			else	/* omatch succeeded on this pattern element */
-				j += patsize(patt, j);
-		return offset;
-	}
-
-	/* patsize -- return size of pattern entry at patt[n] */
-	protected int patsize(StringBuffer patt, int n) {
-		switch(patt.charAt(n)) {
-		case LITCHAR:
-			return 2;
-		case BOL:
-		case EOL:
-		case ANY:
-			return 1;
-		case CCL:
-		case NCCL:
-			return patt.charAt(n+1) + 2;
-		case CLOSURE_ANY:
-			return CLOSIZE;
-		default:
-			System.err.println("in patsize: invalid case, cant happen");
-			return 0;
 		}
-	}
-
-	/* omatch -- match one pattern element at patt[j], return boolean */
-	protected boolean omatch(String line, Int i, StringBuffer patt, int j) {
-		int advance = -1;
-
-		if (line.length() == i.get()+1)
-			return false;
-		else
-			switch(patt.charAt(j)) {
-			case LITCHAR:
-System.out.println("i="+i+", j=" + j);
-				if (line.charAt(i.get()) == patt.charAt(j+1))
-					advance = 1;
-				break;
-			case BOL:
-				if (i.get() == 0)
-					advance = 0;
-			case ANY:
-				if (line.charAt(i.get()) != \0)	// XXX
-					advance = 1;
-				break;
-			case EOL:
-				if (line.charAt(i.get()) == \0)	// XXX
-					advance = 0;
-				break;
-			case CCL:
-				if (locate(line.charAt(i.get()), patt, j+1))
-					advance = 1;
-				break;
-			case NCCL:
-				if (line.charAt(i.get()) != \0 && !locate(line.charAt(i.get()), patt, j+1))
-					advance = 1;
-				break;
-			default:
-				System.err.println("omatch: bad case, cant happen");
-				return false;
-			}
-		if (advance >= 0) {
-			i.incr(advance);
-			return true;
-		} else
-			return false;
+		return !done;
 	}
 
 	/* locate -- look for c in character class at patt[offset] */
@@ -339,6 +210,144 @@ System.out.println("i="+i+", j=" + j);
 		if (a.charAt(i.get()) == t)
 			return \t;
 		return a.charAt(i.get());
+	}
+
+	/** Represent one sub-expression in an RE. */
+	abstract class SE {
+		/** Start matching at i in ln. Increment i as much as matches.
+		 * Return true if valid match, false if not.
+		 */
+		public abstract boolean amatch(String ln, Int i);
+	}
+
+	class SEchar extends SE {
+		char val;
+
+		public SEchar(char ch) { val = ch; }
+
+		public String toString() { return "SEchar(" + val + ); }
+
+		public boolean amatch(String ln, Int i) {
+			if (ln.length() >= i.get())
+				return false;					// end of string
+			i.incr();
+			return ln.charAt(i.get()) == val;
+		}
+	}
+
+	class SEany extends SE {
+		public boolean amatch(String ln, Int i) {
+			if (ln.length() >= i.get())
+				return false;					// end of string
+			i.incr();
+			return true;
+		}
+	}
+
+	class SEbol extends SE {
+		public boolean amatch(String ln, Int i) {
+			if (i.get()>0)
+				return false;
+			// no i.incr() since we dont use any chars in ln
+			return true;
+		}
+	}
+
+	class SEeol extends SE {
+		public boolean amatch(String ln, Int i) {
+			if (i.get() == ln.length() - 1) {
+				// no i.incr() since we dont use any chars in ln
+				return true;
+			}
+			return false;
+		}
+	}
+
+	/** SEccl represents on Character Class */
+	class SEccl extends SE {
+		int len = 0;
+		boolean negate = false;
+		StringBuffer val = new StringBuffer();
+
+		public String toString() {
+			return "SEccl[" + val.toString() + "]";
+		}
+
+		/** Construct a CCL. Some of this code was in "getCCL()" */
+		public SEccl(String arg, Int i) {
+			int jstart;
+
+			i.incr();			/* skip over [ */
+			if (i.get()>=arg.length())
+				throw new RESyntaxException("pattern ends with [");
+			if (arg.charAt(i.get()) == NEGATE) {
+				negate = true;
+				i.incr();
+			}
+			// Expand the range
+			len = doDash(arg, i);
+			if (arg.charAt(i.get()) != CCLEND)
+				throw new RESyntaxException("CCL ends without ]");
+		}
+
+		/* doDash - expand dash shorthand set at src[i] to end of dest.
+		 * @return number of characters appended to dest.
+		 */
+		protected int doDash(String src, Int i) {
+
+			int startLen = val.length();
+
+			while (src.charAt(i.get()) != CCLEND && i.get()<src.length()) {
+				if (src.charAt(i.get()) == LITCHAR)
+					val.append(esc(src, i));
+				else if (src.charAt(i.get()) != DASH)
+					val.append(src.charAt(i.get()));
+				else if (val.length() == 0 || src.length() == i.get()+1)
+					val.append(DASH);	/* literal - */
+				else if (Character.isLetterOrDigit(src.charAt(i.get()-1)) && 
+					Character.isLetterOrDigit(src.charAt(i.get()+1)) &&
+					src.charAt(i.get()-1) <= src.charAt(i.get()+1)) {
+					for (int k = src.charAt(i.get()-1)+1; k <= src.charAt(i.get()+1); k++)
+						val.append((char)k);
+					i.incr();
+				}
+				else
+					val.append(DASH);
+				i.incr();
+			}
+			return val.length() - startLen;
+		}
+
+		public boolean amatch(String ln, Int i) {
+			// match CCLs here
+			for (int k = 0; k<val.length(); k++)
+				if (ln.charAt(i.get()) == k)
+					return true;
+			return false;
+		}
+	}
+
+	/** SEclos represents one Closure */
+	class SEclos extends SE {
+		/** Is this closure *, + or ? */
+		int type;
+		/** What SubExpression is this a closure of? */
+		SE target;
+
+		/** Make me printable */
+		public String toString() {
+			return "SEclos(" + type + "->" + target + ")";
+		}
+
+		/** Construct a Closure */
+		public SEclos(char ty, SE ta) {
+			type = ty;
+			target = ta;
+		}
+
+		public boolean amatch(String ln, Int i) {
+			throw new IllegalArgumentException("Closure amatch not written!");
+		}
 	}
 //+
 }

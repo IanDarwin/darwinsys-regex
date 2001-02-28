de048914b2a49e8fb166413399782be02a8c8292
IanDarwin <>
2001-02-28 17:15:46 +0000

    RESyntaxException now unchecked, as expected in jakarta-regexp-1.3.

--- src/main/java/darwinsys/regex/RESyntaxException.java
+++ src/main/java/darwinsys/regex/RESyntaxException.java
@@ -1,11 +1,15 @@
 package com.darwinsys.regexp;
 
-/** An exception that represents a syntax error in an RE pattern. */
-public class RESyntaxException extends Exception {
+/** An unchecked Exception representing a syntax error 
+ * in an RE pattern.
+ */
+public class RESyntaxException extends RuntimeException {
+
 	/** Construct an RESyntaxException with a message */
 	public RESyntaxException(String mesg) {
 		super(mesg);
 	}
+
 	/** Construct a user-hostile RESyntaxException with no message. */
 	public RESyntaxException() {
 		super();
